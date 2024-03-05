d496b1332ef84921897b36fe124b72fe5b1839c1
Joas Blokhuis <joas.blokhuis@hva.nl>
2024-03-05 12:18:58 +0100

    Kara's vale town square room

--- src/api/src/rooms/KarasValeTownSquareRoom.ts
+++ src/api/src/rooms/KarasValeTownSquareRoom.ts
@@ -1,6 +1,9 @@
 import { ActionResult } from "../base/actionResults/ActionResult";
 import { TextActionResult } from "../base/actionResults/TextActionResult";
 import { Room } from "../base/gameObjects/Room";
+import { GameObject } from "../base/gameObjects/GameObject";
+import { Action } from "../base/actions/Action";
+import { NavigationActions } from "../actions/Navigate";
 
 export const KarasValeTownSquareRoomAlias: string = "KVTownSquare";
 
@@ -17,7 +20,20 @@ export class KarasValeTownSquareRoom extends Room {
         return ["KVTownSquare1"];
     }
 
+    public actions(): Action[] {
+        return [new NavigationActions()];
+    }
+
     public examine(): ActionResult | undefined {
         return new TextActionResult(["In front of you is a small town named Kara's Vale."]);
+    }
+
+    public custom(alias: string, _gameObjects?: GameObject[]): ActionResult | undefined {
+        if (alias === "Navigate-North") {
+            return new TextActionResult([
+                "You move to the center of the town, and see there is a small tavern and a blacksmith.",
+            ]);
+        }
+        return undefined;
     }
 }
