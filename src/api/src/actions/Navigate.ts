37dd414c64c4f4ecdbe7d5a6a9e32576aa9321c4
Joas Blokhuis <joas.blokhuis@hva.nl>
2024-03-05 14:12:03 +0100

    navigatie classes voor de windrichtingen

--- src/api/src/actions/Navigate.ts
+++ src/api/src/actions/Navigate.ts
@@ -1,19 +1,51 @@
-import { ActionResult } from "../base/actionResults/ActionResult";
 import { CustomAction } from "../base/actions/CustomAction";
 import { Action } from "../base/actions/Action";
 
-export const NavigationActionsAlias: string = "Navigate-North";
+export const NavigateNorthAlias: string = "NavigateNorth";
+export const NavigateEastAlias: string = "NavigateEast";
+export const NavigateSouthAlias: string = "NavigateSouth";
+export const NavigateWestAlias: string = "NavigateWest";
 
-export interface Navigate {
-    navigate(): ActionResult | undefined;
-}
-
-export class NavigationActions extends Action {
+// class om naar het noorden te navigeren
+export class NavigationNorth extends Action {
     public constructor() {
-        super(NavigationActionsAlias, "Go North", false);
+        super(NavigateNorthAlias, "Go North", false);
     }
 
-    public NavigationActions(): Action[] {
-        return [new CustomAction("Navigate-North", "Go North", false)];
+    public NavigationNorth(): Action[] {
+        return [new CustomAction("NavigateNorth", "Go North", false)];
+    }
+}
+
+// class om naar het oosten te navigeren
+export class NavigationEast extends Action {
+    public constructor() {
+        super(NavigateEastAlias, "Go East", false);
+    }
+
+    public NavigationEast(): Action[] {
+        return [new CustomAction("NavigateEast", "Go East", false)];
+    }
+}
+
+// class om naar het zuiden te navigeren
+export class NavigationSouth extends Action {
+    public constructor() {
+        super(NavigateSouthAlias, "Go South", false);
+    }
+
+    public NavigationSouth(): Action[] {
+        return [new CustomAction("NavigateSouth", "Go South", false)];
+    }
+}
+
+// class om naar het westen te navigeren
+export class NavigationWest extends Action {
+    public constructor() {
+        super(NavigateWestAlias, "Go West", false);
+    }
+
+    public NavigationWest(): Action[] {
+        return [new CustomAction("NavigateWest", "Go West", false)];
     }
 }
