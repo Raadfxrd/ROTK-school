a7b4e6a560fbb934271cae4516c0ae8998edb37a
Joas Blokhuis <joas.blokhuis@hva.nl>
2024-03-06 12:21:55 +0100

    Functies weg gehaald die niet nodig waren

--- src/api/src/actions/NavigateAction.ts
+++ src/api/src/actions/NavigateAction.ts
@@ -1,19 +1,14 @@
-import { CustomAction } from "../base/actions/CustomAction";
 import { Action } from "../base/actions/Action";
 
 export const NavigateNorthAlias: string = "NavigateNorth";
 export const NavigateEastAlias: string = "NavigateEast";
 export const NavigateSouthAlias: string = "NavigateSouth";
 export const NavigateWestAlias: string = "NavigateWest";
-
+export const NavigateBlacksmithAlias: string = "KVBlacksmith";
 // class om naar het noorden te navigeren
 export class NavigationNorth extends Action {
     public constructor() {
         super(NavigateNorthAlias, "Go North", false);
-    }
-
-    public NavigationNorth(): Action[] {
-        return [new CustomAction("NavigateNorth", "Go North", false)];
     }
 }
 
@@ -22,20 +17,12 @@ export class NavigationEast extends Action {
     public constructor() {
         super(NavigateEastAlias, "Go East", false);
     }
-
-    public NavigationEast(): Action[] {
-        return [new CustomAction("NavigateEast", "Go East", false)];
-    }
 }
 
 // class om naar het zuiden te navigeren
 export class NavigationSouth extends Action {
     public constructor() {
         super(NavigateSouthAlias, "Go South", false);
-    }
-
-    public NavigationSouth(): Action[] {
-        return [new CustomAction("NavigateSouth", "Go South", false)];
     }
 }
 
@@ -44,8 +31,12 @@ export class NavigationWest extends Action {
     public constructor() {
         super(NavigateWestAlias, "Go West", false);
     }
+}
 
-    public NavigationWest(): Action[] {
-        return [new CustomAction("NavigateWest", "Go West", false)];
+// class om naar de algemene blacksmith kamers te navigeren
+
+export class NavigationBlacksmith extends Action {
+    public constructor() {
+        super(NavigateBlacksmithAlias, "Blacksmith", false);
     }
 }
