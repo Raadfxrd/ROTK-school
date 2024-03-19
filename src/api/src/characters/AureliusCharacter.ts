57a6d3fc958237ac33f5d93aaff6be316dbc3672
Joas Blokhuis <joas.blokhuis@hva.nl>
2024-03-19 17:33:48 +0100

    meer aurelius character dialogue

--- src/api/src/characters/AureliusCharacter.ts
+++ src/api/src/characters/AureliusCharacter.ts
@@ -32,18 +32,145 @@ export class AureliusCharacter extends Character implements Examine {
                 this,
                 ["Aurelius: And who might you be looking for?"],
                 [
-                    new TalkChoiceAction(6, "You do not need to know who it is."),
-                    new TalkChoiceAction(3, "I am looking for the princess."),
+                    new TalkChoiceAction(3, "You do not need to know who it is."),
+                    new TalkChoiceAction(4, "I am looking for the princess."),
                 ]
             );
         }
+
         if (choiceId === 2) {
             return new TalkActionResult(
                 this,
-                ["Aurelius: Well i'm afraid I can't help you then, but I do know of someone who might."],
-                [new TalkChoiceAction(97, "Please, where can i find this person.")]
+                ["Aurelius: People pass through here all the time.", "Although they hardly ever stay."],
+                [
+                    new TalkChoiceAction(5, "Specifically people on horses."),
+                    new TalkChoiceAction(6, "Anybody notable?"),
+                ]
             );
         }
+
+        if (choiceId === 3) {
+            return new TalkActionResult(
+                this,
+                ["Aurelius: Well i'm afraid I can't help you then."],
+                [
+                    new TalkChoiceAction(4, "Fine, I am looking for the princess."),
+                    new TalkChoiceAction(97, "Please my quest is of utmost importance."),
+                    new TalkChoiceAction(99, "bye!"),
+                ]
+            );
+        }
+
+        if (choiceId === 4) {
+            return new TalkActionResult(
+                this,
+                ["Aurelius: Oh my! What happened?"],
+                [
+                    new TalkChoiceAction(7, "She was kidnapped"),
+                    new TalkChoiceAction(8, "That is not your concern."),
+                ]
+            );
+        }
+
+        if (choiceId === 5) {
+            return new TalkActionResult(
+                this,
+                [
+                    "Aurelius: Well I did not see anybody on horses myself, but I did hear the sound of them this afternoon.",
+                    "Perhaps someone else saw something.",
+                ],
+                [new TalkChoiceAction(99, "I'll ask around then."), new TalkChoiceAction(99, "bye!")]
+            );
+        }
+
+        if (choiceId === 6) {
+            return new TalkActionResult(
+                this,
+                [
+                    "Aurelius: I haven't seen anybody recently.",
+                    "You should ask Jaina, she keeps a watchful eye.",
+                ],
+                [new TalkChoiceAction(99, "Thank you, I shall ask her.")]
+            );
+        }
+
+        if (choiceId === 7) {
+            return new TalkActionResult(
+                this,
+                [
+                    "Aurelius: My goodness! It would seem the quality of the kingsguard has dwindled these days.",
+                ],
+                [
+                    new TalkChoiceAction(9, "Watch your mouth old man."),
+                    new TalkChoiceAction(10, "They caught us by surprise."),
+                    new TalkChoiceAction(
+                        11,
+                        "Well, now that you understand the urgency of my quest then tell me everything you know."
+                    ),
+                ]
+            );
+        }
+
+        if (choiceId === 8) {
+            return new TalkActionResult(
+                this,
+                [
+                    "Aurelius: Very well, I understand that you can't give away all your secrets.",
+                    "I know someone who might be able to help you.",
+                ],
+                [
+                    new TalkChoiceAction(97, "Please, where can I find this person."),
+                    new TalkChoiceAction(97, "Tell me where they are and I'll reward you handsomely."),
+                ]
+            );
+        }
+
+        if (choiceId === 9) {
+            return new TalkActionResult(
+                this,
+                [
+                    "Aurelius: Alright, Alright, there's no need for this to get violent.",
+                    "You will want to seek out Kara. She lives in the forest. ",
+                    "But beware, for if you can not answer her riddles there will be a price to pay. ",
+                    "Take this aswell, you shall need it to summon her. ",
+                    "<He gives you a whistle.>",
+                ],
+                [new TalkChoiceAction(98, "Thank you.")]
+            );
+        }
+
+        if (choiceId === 10) {
+            return new TalkActionResult(
+                this,
+                [
+                    "Aurelius: Sloppy.",
+                    "Well then, you will want to seek out Kara. She lives in the forest.",
+                    "But beware, for if you can not answer her riddles there will be a price to pay. ",
+                    "Take this aswell, you shall need it to summon her. ",
+                    "<He gives you a whistle.>",
+                ],
+                [new TalkChoiceAction(98, "Thank you.")]
+            );
+        }
+
+        if (choiceId === 11) {
+            return new TalkActionResult(
+                this,
+                [
+                    "Aurelius: Fine.",
+                    "You will want to seek out Kara. She lives in the forest. ",
+                    "But beware, for if you can not answer her riddles there will be a price to pay. ",
+                    "Take this aswell, you shall need it to summon her. ",
+                    "<He gives you a whistle.>",
+                ],
+                [new TalkChoiceAction(98, "Thank you.")]
+            );
+        }
+
+        if (choiceId === 12) {
+            return new TextActionResult(["You blow it of course!"]);
+        }
+
         if (choiceId === 97) {
             return new TalkActionResult(
                 this,
@@ -63,16 +190,28 @@ export class AureliusCharacter extends Character implements Examine {
 
             return new TextActionResult([""]);
         }
+
         if (choiceId === 99) {
             return new TextActionResult([""]);
         }
+
+        if (this.playerSession.hasWhistle === true) {
+            return new TalkActionResult(
+                this,
+                ["Aurelius: I've nothing more of importance to say to you."],
+                [
+                    new TalkChoiceAction(12, "What do i do with the whistle?"),
+                    new TalkChoiceAction(99, "Fine."),
+                ]
+            );
+        }
         return new TalkActionResult(
             this,
-            ["Hello there"],
+            ["Aurelius: Hello there"],
             [
                 new TalkChoiceAction(1, "I'm looking for someone"),
                 new TalkChoiceAction(2, "Has anybody passed through this village recently?"),
-                new TalkChoiceAction(10, "Bye!"),
+                new TalkChoiceAction(99, "Bye!"),
             ]
         );
     }
